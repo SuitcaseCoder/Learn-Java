4a670eeda0a8ce2abae37eefa7aad600be626361
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-04-30 12:42:30 -0500

    getting started

---
+++ cu-curriculum/src/HelloWorld.java
@@ -0,0 +1,10 @@
+// // "hello world class created
+public class HelloWorld {
+
+    // // program that outputs 'hello world' to the console
+    public static void main (String[] args) {
+        // // actually prints it out to the system in a new line
+        // // an 'out' folder gets created
+        System.out.println("hello, World!");
+    }
+}
