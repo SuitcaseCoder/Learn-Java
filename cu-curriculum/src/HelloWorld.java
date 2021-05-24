3292ee26ab0440be1438780e5eb90e71f52fd0a6
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-05-24 16:05:18 -0500

    java-cu-exercises

--- cu-curriculum/src/HelloWorld.java
+++ cu-curriculum/src/HelloWorld.java
@@ -6,32 +6,33 @@ public class HelloWorld {
         // // actually prints it out to the system in a new line
         // // an 'out' folder gets created
         System.out.println("hello, World!");
+//
+//        /* Exercises - https://java.codeup.com/java-i/syntax-types-and-variables/#exercises */
+//
+//        // // 1.
+//        int myFavoriteNumber = 18;
+//        System.out.println(myFavoriteNumber);
+//
+//        // // 2.
+//        String myString = "The Lumineers is still one of my favorite bands";
+//        System.out.println(myString);
 
-        /* Exercises */
-        // // 1.
-        int myFavoriteNumber = 18;
-        System.out.println(myFavoriteNumber);
-
-        // // 2.
-        String myString = "The Lumineers is still one of my favorite bands";
-        System.out.println(myString);
-
-        // // 5. - 9
-        long myNumber = 123L;
-        System.out.println(myNumber);
-
-        float myOtherNumber = 3.14f;
-        // // adding the f to the end of the number says to read it as a float (?)
-        System.out.println(myOtherNumber);
+//        // // 5. - 9
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+//
+//        float myOtherNumber = 3.14f;
+//        // // adding the f to the end of the number says to read it as a float (?)
+//        System.out.println(myOtherNumber);
 
         // // 10.
-        int x = 5;
-        System.out.println(x++); // 5
-        System.out.println(x); // 6
-
-        int y = 5;
-        System.out.println(++y); // 6
-        System.out.println(y); // 6
+//        int x = 5;
+//        System.out.println(x++); // 5
+//        System.out.println(x); // 6
+//
+//        int y = 5;
+//        System.out.println(++y); // 6
+//        System.out.println(y); // 6
 
         // // 12.
 //        String theNumberThree = "three";
@@ -40,12 +41,20 @@ public class HelloWorld {
 
 //        int three = (int) "three";
 
-        //
-        int z = 4;
-//        z = z + 5;
 
-        z += 5;
-        System.out.println(z);
+//  // 13.
+        int x = 4;
+//        x = x + 5;
+
+        x += 5;
+        System.out.println(x);
+
+//
+        //    int x = 3;
+        //int y = 4;
+        //y = y * x;
+        
 
     }
+
 }
