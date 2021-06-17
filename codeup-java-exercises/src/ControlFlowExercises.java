17684dc3787b41c2a42d2bd135df2f27715a6d68
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-17 13:59:34 -0500

    control flow notes

--- codeup-java-exercises/src/ControlFlowExercises.java
+++ codeup-java-exercises/src/ControlFlowExercises.java
@@ -2,21 +2,87 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//         string comparison:
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Continue? [y/N] ");
-        String userInput = sc.next();
+////  string comparison:
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Continue? [y/N] ");
+//        String userInput = sc.next();
+//
+//// Don't do this!
+//        boolean confirmation = userInput == "y";
+//        System.out.println(confirmation);
+//
+////  Do this instead:
+//        boolean rightConfirmation = userInput.equals("y");
+//        System.out.println(rightConfirmation);
 
-// Don't do this!
-        boolean confirmation = userInput == "y";
-        System.out.println(confirmation);
+////  If and if/else/if Statement syntax
+////        if (/* condition */) {
+////            // conditional code that will only execute if the condition
+////            // is met to be true
+////        }
+//
+//        boolean someCondition = false;
+//        boolean someOtherCondition = true;
+//
+//        if (someCondition) {
+//            // executes if someCondition is true
+//        } else if (someOtherCondition) {
+//            // executes if someOtherCondition is true *and* all of the previous ifs in
+//            // this block were false
+//        } else {
+//            // executes if none of the conditions checked in this block were true
+//        }
+//
+////        use Integer to save a number variable
+//        Integer eight = 8;
+//        Integer ten = 10;
+//        if(eight < ten){
+//            System.out.println("eight is less than ten");
+//        }
+        
+////  Switch Statement Syntax
+//        int caseSwitch = 1;
+////    comparing the caseSwitch to all the different cases (1, 2, etc.)
+//        switch (caseSwitch) {
+//            case 1:
+//                System.out.println("Case 1");
+//                // Fall through!
+//            case 2:
+//                System.out.println("Case 2");
+//                break;
+//            default:
+//                System.out.println("Default case");
+//                break;
+//        }
 
-//  Do this instead:
-        boolean rightConfirmation = userInput.equals("y");
-        System.out.println(rightConfirmation);
+////    While Loops
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
 
+////      Do While Loop
+//        do {
+//            System.out.println("You will see this despite the condition!");
+//        } while (false);
+        
+////      For Loop
+////        for(initializing point; condition; iteration/update)
+//        for(int i = 0; i < 10; i += 1) {
+////            execute code at each iteration
+//            System.out.println("i is " + i);
+//        }
+
+//     EXERCISES
+        
+    
+    
+    
+    
+    
+    
+    
     }
-
-
 
 }
